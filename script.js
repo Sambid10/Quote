8bcb51a77b69d6c106294f1a1e8b1c0d11ddca81
Sambid10 <sambidshk10@gmail.com>
2025-05-08 15:14:39 +0545

    added basic ui to the app

--- script.js
+++ script.js
@@ -15,10 +15,14 @@ async function getQuotes(url) {
         throw new Error(`${res.status}`)
     }
     const data = await res.json();
-    console.log(data)
+    const quote=data[0] //getting the quote only & not author
+    console.log(quote)
+    const quote_container=document.getElementById("quote_contain")
+    quote_container.innerHTML=`${quote.quote}`
   } catch (error) {
     console.error("Error fetching quote:", error);
   }
 }
-
-getQuotes(api_url);
+document.getElementById("fetchbtn").addEventListener("click",function(){
+    getQuotes(api_url)
+})
