b6c23f01d4e775cbe1f3cb7f1ec8f5244d7c18cd
Sambid10 <sambidshk10@gmail.com>
2025-05-08 16:41:19 +0545

    removed api and just used object of arrays

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-const api_url = "https://api.api-ninjas.com/v1/quotes";
-const api_key="GJwouyHqjvXffxjfolKCuw==IQrA9roqvqBSBkDu" //should be in .env btw
-
-
-async function getQuotes(url) {
-  try {
-    const res = await fetch(url,{
-        method:"GET",
-        headers:{
-            "X-Api-Key": api_key,
-            "Content-Type": "application/json",
-        }
-    });
-    if(!res.ok){
-        throw new Error(`${res.status}`)
-    }
-    const data = await res.json();
-    const quote=data[0] //getting the quote only & not author
-    console.log(quote)
-    const quote_container=document.getElementById("quote_contain")
-    quote_container.innerHTML=`${quote.quote}`
-  } catch (error) {
-    console.error("Error fetching quote:", error);
-  }
+const quotesData = {
+    inspire: [
+      { quote: "Believe in yourself.", author: "Unknown" },
+      { quote: "Dream big and dare to fail.", author: "Norman Vaughan" }
+    ],
+    success: [
+      { quote: "Success is no accident.", author: "Pele" },
+      { quote: "Success usually comes to those who are too busy to be looking for it.", author: "Henry David Thoreau" }
+    ],
+    science: [
+      { quote: "The important thing is to never stop questioning.", author: "Albert Einstein" },
+      { quote: "Science is a way of thinking much more than it is a body of knowledge.", author: "Carl Sagan" }
+    ],
 }
-document.getElementById("fetchbtn").addEventListener("click",function(){
-    getQuotes(api_url)
-})
+  
+  let currentSelectedcat = "success";
+  let currentSelectedindx = 0;
+  
+  const quotetxt = document.getElementById("quote-txt");
+  const quoteauth = document.getElementById("quote-auth");
+  
+  function DisplayQuote(category) {
+    const currentquote = quotesData[category][currentSelectedindx];
+    quotetxt.textContent = `"${currentquote.quote}"`;
+    quoteauth.textContent = `— ${currentquote.author}`;
+  }
+
+  document.getElementById("category-select").addEventListener("change",function(e){
+    categoryselected=e.target.value;
+    currentSelectedindx=0
+    DisplayQuote(categoryselected)
+  })
+  DisplayQuote(currentSelectedcat);
+  
